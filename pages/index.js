81d7e9d343f17c6196ec93fd65035d243c9de893
Johannes Langosch <johannes.langosch@googlemail.com>
2022-12-01 20:14:07 +0100

    06 database query with timestamp

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import prisma from "lib/prisma";
+import { getDays } from "lib/data";
 
-export default function Home() {
+export default function Home({ days }) {
   const { data: session, status } = useSession();
   const router = useRouter();
 
@@ -14,15 +16,43 @@ export default function Home() {
   }
 
   return (
-    <div>
-      {session ? (
+    <div className="h-screen bg-green-500 text-red-700">
+      {/* {session ? (
         <p className="text-green-500">You are logged in!</p>
       ) : (
         <p>
           You are not logged in 😞
           <a href="/api/auth/signin">Please sign in</a>
         </p>
-      )}
+      )} */}
+
+      {days.map((day, index) => (
+        <div key={index}>
+          <p className="text-blue-500" key={index}>
+            {day.singleday}
+          </p>
+          {day.timestamps.map((timestamp, index) => (
+            <p
+              className="ml-4 text-xs text-blue-800 bg-slate-400"
+              key={index}>
+              {timestamp.stamp}
+            </p>
+          ))}
+        </div>
+      ))}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const datefrom = "2022-11-28";
+  const dateto = "2022-12-01";
+  let days = await getDays(datefrom, dateto, prisma);
+  days = JSON.parse(JSON.stringify(days));
+
+  return {
+    props: {
+      days,
+    },
+  };
 }
---
+++ pages/show.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function show() {
+  return (
+    <div className="bg-green-500 h-screen w-full">SHOW</div>
+  );
+}
+
+export default show;
