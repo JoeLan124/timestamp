5ed3a8996393fd62ca61de659b112b7876ae8617
Johannes Langosch <johannes.langosch@googlemail.com>
2023-02-05 20:31:53 +0100

    27 finalized pagewithmodal

--- components/modal.js
+++ components/modal.js
@@ -11,17 +11,21 @@ const modal = ({ setModalOn, setChoice }) => {
   };
 
   return (
-    <div className="backdrop-blur-lg opacity-80 fixed inset-0 z-50">
+    <div className=" opacity-80 bg-gray-100 backdrop-blur-sm   fixed inset-0 z-50">
       <div className="flex h-screen justify-center items-center">
         <div className="flex-col justify-center bg-orange-200 py-12 px-24 border-4 border-white rounded-xl">
           <div className="flex text-lg mb-10">
             Are you sure?
           </div>
           <div className="flex">
-            <button className="rounded px-4 py-2 text-white bg-orange-300">
+            <button
+              className="rounded px-4 py-2 text-white bg-orange-300"
+              onClick={handleOKClick}>
               Yes
             </button>
-            <button className="rounded px-4 py-2 ml-4 text-white bg-orange-500">
+            <button
+              className="rounded px-4 py-2 ml-4 text-white bg-orange-500"
+              onClick={handleCancelClick}>
               No
             </button>
           </div>
--- pages/pagewithmodal.js
+++ pages/pagewithmodal.js
@@ -1,13 +1,38 @@
 import React from "react";
 import Modal from "../components/modal";
+import { useState } from "react";
 
 function pagewithmodal() {
+  const [modalOn, setModalOn] = useState(false);
+  const [choice, setChoice] = useState(false);
+
+  const clicked = () => {
+    setModalOn(true);
+  };
   return (
-    <div className="flex justify-center">
-      <div className="flex cursor-pointer justify-center w-1/3 bg-blue-400 p-4 m-6 rounded-md text-white">
-        bite me
+    <div>
+      <div className="flex justify-center items-center">
+        <div
+          className="flex justify-center cursor-pointer w-1/3 bg-blue-400 p-4 m-6 rounded-md text-white"
+          onClick={clicked}>
+          Bite me
+        </div>
       </div>
-      <Modal />
+
+      {choice && (
+        <div className="flex justify-center">
+          <div className="flex justify-center w-1/3 bg-red-400 m-4 p-6 text-lg rounded-md text-white">
+            You have been biten!!!
+          </div>
+        </div>
+      )}
+
+      {modalOn && (
+        <Modal
+          setModalOn={setModalOn}
+          setChoice={setChoice}
+        />
+      )}
     </div>
   );
 }
